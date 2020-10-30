c7eab8e92f2149c7c5cab9b8fc0b68100da0090f
LeandroParisi <lepcbelisario@gmail.com>
2020-10-30 13:58:51 -0300

    first commit

---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <section className='login-page'>
+        <main className='login-container'>
+
+        </main>
+      </section>
+    )
+  }
+}
